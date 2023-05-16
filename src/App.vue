82977834e4018baf9336e69be50a63ab1cc9a557
reinnerh <leonardo.reinner@4network.com>
2023-05-16 10:52:22 -0300

    FInaliza criação de posts

--- src/App.vue
+++ src/App.vue
@@ -16,29 +16,65 @@ export default {
           content: "Conteudo não tão bacana!",
         },
       ],
+      formData: {
+        title: "",
+        content: "",
+        /*para pegar as infos colocadas*/
+      },
     };
   },
   methods: {
-    handleClick(event){
-      event.preventDefault();
-    }
-  }
+    handleClick(event) {
+      console.log(this.formData);
+      
+      //adicionar o post para lista de posts:
+      const now = new Date()
+      const dataDaPostagem = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`;
+    
+
+
+
+      //metodo 1:
+      /* this.posts[this.posts.length] = {
+        title: this.formData.title,
+        content: this.formData.content,
+      } */
+      //metodo 2:
+      this.posts.push({
+        title: this.formData.title,
+        content: this.formData.content,
+        datetime: dataDaPostagem,
+      });
+
+      this.formData = {
+        title: "",
+        content: "",
+      };
+    },
+    handleInputChange(event) {
+      const { name, value } = event.target;
+      this.formData[name] = value;
+    },
+    /* para tratar os inputs */
+  },
 };
 </script>
 
 <template>
   <div id="lista-posts">
     <div class="post" v-for="x in posts" :key="posts.title">
-        <h3>{{x.title}}</h3>
-        <h4>{{ x.datetime }}</h4>
-        <p>{{ x.content }}</p>
+      <h3>{{ x.title }}</h3>
+      <h4>{{ x.datetime }}</h4>
+      <p>{{ x.content }}</p>
     </div>
   </div>
 
-  <form action="">
-    <input name="title">
-    <textarea name="content" id="" cols="" rows="10"></textarea>
-    <button type="submit" @click="handle">Criar</button>
+  <form>
+
+    <!-- <input :value="text" @input="event => text = event.target.value"> -->
+    <input v-model="formData.title" placeholder="Título"/>
+    <textarea v-model="formData.content" placeholder="Escreva seu post aqui..." rows="10" cols="50"></textarea>
+    <button type="button" @click="handleClick">Criar</button>
   </form>
 
   <RouterView />
@@ -46,14 +82,29 @@ export default {
 
 <style scoped>
 
+#lista-posts{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.post {
+  height: 150px;
+  width: 300px;
+  margin: 10px;
+}
+
 form {
   display: flex;
   flex-direction: column;
-
+  justify-content: center;
+  align-items: center;
 }
 
 form > * {
   margin: 1rem;
+  border-color: transparent;
 }
-
 </style>
