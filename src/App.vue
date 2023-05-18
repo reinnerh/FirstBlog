d44f9d6766c7d4435886cd1255dd74022e9ceb45
reinnerh <leonardo.reinner@4network.com>
2023-05-18 09:57:19 -0300

    estilizacao-nav-e-detalhes-nos-posts

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,35 @@ export default {
 </script>
 
 <template>
-  <RouterLink to="/">Home</RouterLink>
-  <RouterLink to="/create">Novo Post</RouterLink>
+  <header>
+    <nav>
+      <RouterLink to="/" class="nav-link">Home</RouterLink>
+      <RouterLink to="/create" class="nav-link">Novo Post</RouterLink>
+    </nav>
+  </header>
 
   <RouterView :posts="posts" @create-post="addPost" />
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.nav-link {
+  display: inline-block;
+  padding: 10px 20px;
+  margin-top: 10px;
+  font-size: 16px;
+  text-decoration: none;
+  color: #333;
+  background-color: #dddddd;
+  border-radius: 5px;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.nav-link:hover {
+  background-color: #6e88a7;
+}
+</style>
