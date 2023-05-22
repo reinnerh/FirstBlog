1e542eca27f63688b8534fe7eebd6ef720f21712
reinnerh <leonardo.reinner@4network.com>
2023-05-22 14:53:39 -0300

    cria detail view

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,7 +1,41 @@
 <script>
+export default {
+  props: {
+    posts: Array,
+  },
+  data(){
+    const idData = this.$route.params.id;
+    return {
+      id: idData,
+      post: this.posts[idData]
+    }
+  },
+};
 </script>
-
 <template>
   <main>
+    <h2>{{ post.title }}</h2>
+    <h4>{{ post.datetime }}</h4>
+    <textarea readonly :value="post.content" rows="10" cols="50"></textarea>
   </main>
 </template>
+
+<style scoped>
+textarea {
+  resize: none;
+  outline: none;
+  border: black;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 80%;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
